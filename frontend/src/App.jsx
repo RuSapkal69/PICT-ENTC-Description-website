8bb2a66b5f867ed1d151f8055f83f2736b38b7c3
Rushikesh Sapkal <rushikeshsapkal930@gmail.com>
2025-01-26 23:58:47 +0530

    PICT ENTC website repo created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-5xl items-center justify-center flex h-screen text-white bg-blue-500'>
+      PICT ENTC Department Website
+    </div>
+  )
+}
+
+export default App
